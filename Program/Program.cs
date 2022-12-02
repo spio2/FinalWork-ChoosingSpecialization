cab514d4b9fb29b3add20d439bc917e30e3f3cfd
LiAnna <spio@mail.ru>
2022-12-02 19:49:18 +0300

    Добавила решение задачи Program

---
+++ Program/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+
+string[] ArrayMaxTextThreeLength = { "Всё", "будет", "не", "надо", "спешить", "так", "!"};
+string str = string.Join(", ", ArrayMaxTextThreeLength);
+Console.WriteLine($"[{str}] -> ");
+
+for (int i = 0; i < ArrayMaxTextThreeLength.Length; i++)
+
+{
+    if (ArrayMaxTextThreeLength[i].Length <= 3)
+    {
+        if (i != ArrayMaxTextThreeLength.Length - 1)
+            Console.Write($"{ArrayMaxTextThreeLength[i]} ");
+
+        else
+            Console.Write($"{ArrayMaxTextThreeLength[i]}");
+    }
+}
+
